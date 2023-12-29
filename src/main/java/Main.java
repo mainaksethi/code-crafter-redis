aee0562083cd3ec365ed839d006da8fb37481f08
mainaksethi <mainak@spotnana.com>
2023-12-29 16:38:40 +0530

    pass 2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,55 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        int port = 6379;
-        try {
-          serverSocket = new ServerSocket(port);
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
+class ClientHandler implements Runnable { // Runnable enables threading
+    private Socket clientSocket;
+    public ClientHandler(Socket socket) { this.clientSocket = socket; }
+    @Override
+    public void run() {
+        System.out.println("Thread is getting called");
+        try (PrintWriter printWriter =
+                     new PrintWriter(this.clientSocket.getOutputStream())) {
+            printWriter.print(HttpResponse.getPong);
+            printWriter.flush();
+            System.out.println("Pong is sent.");
         } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
+            System.out.println("There is an exception");
             System.out.println("IOException: " + e.getMessage());
-          }
         }
-  }
+    }
+}
+
+class HttpResponse {
+    public static String getPong = "+PONG\r\n";
+}
+class HttpServer {
+    private int port;
+
+    public HttpServer(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is listening on port " + port);
+            while (true) {
+                System.out.println("aya");
+                Socket clientSocket = serverSocket.accept(); // wait for new connections
+                new Thread(new ClientHandler(clientSocket))
+                        .start(); // run on a new thread
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program is starting");
+        new HttpServer(6379).start();
+    }
 }
--- src/main/java/SocketClientExample.java
+++ src/main/java/SocketClientExample.java
@@ -1,6 +1,4 @@
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -19,18 +17,23 @@ public class SocketClientExample {
             //establish socket connection to server
             socket = new Socket(host.getHostName(), 6379);
             //write to socket using ObjectOutputStream
-            oos = new ObjectOutputStream(socket.getOutputStream());
-            System.out.println("Sending request to Socket Server");
-            if (i==4) oos.writeObject("exit");
-            else oos.writeObject(""+i);
-            //read the server response message
-            ois = new ObjectInputStream(socket.getInputStream());
-            String message = (String) ois.readObject();
-            System.out.println("Message: " + message);
-            //close resources
-            ois.close();
-            oos.close();
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
             Thread.sleep(100);
+
+            BufferedReader stdIn = new BufferedReader(
+                    new InputStreamReader(System.in));
+            String userInput;
+
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+            out.close();
+            in.close();
+            stdIn.close();
+            socket.close();
         }
     }
 }
